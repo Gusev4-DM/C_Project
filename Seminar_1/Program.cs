f952bee0b49df880f6db22d738447ca616555a01
Dmitriy Gusev <gusdima228@gmail.com>
2023-03-06 21:16:50 +0300

    changes Seminar_1

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,32 +1,32 @@
-﻿// Console.Write("Введите целое число: ");
+﻿Console.Write("Введите целое число: ");
 
-// // string userInputStr = Console.ReadLine() ?? "";
-// // int userNumber = int.Parse(userInputStr);
+// string userInputStr = Console.ReadLine() ?? "";
+// int userNumber = int.Parse(userInputStr);
 
-// int userNumber = int.Parse(Console.ReadLine() ?? ""); // эффективнее в одну строку
+int userNumber = int.Parse(Console.ReadLine() ?? ""); // эффективнее в одну строку
 
-// // int result = userNumber * userNumber;
-// int result = (int)Math.Pow(userNumber, 2); // используем бибилиотеку Math, она возвращает вещественный тип данных, требуется перевод в int
+// int result = userNumber * userNumber;
+int result = (int)Math.Pow(userNumber, 2); // используем бибилиотеку Math, она возвращает вещественный тип данных, требуется перевод в int
 
-// Console.WriteLine($"{userNumber} -> {result}"); // интерполяция строк
-// Console.WriteLine(userNumber + " -> " + result); // конкатенация строк
+Console.WriteLine($"{userNumber} -> {result}"); // интерполяция строк
+Console.WriteLine(userNumber + " -> " + result); // конкатенация строк
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Проверка является ли введенное число квадратом другого числа
 
-// Console.Write("Введите число \"а\" число: ");
-// int userNumberA = int.Parse(Console.ReadLine() ?? "");
-// Console.Write("Введите квадрат числа \"а\": ");
-// int userNumberB = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите число \"а\" число: ");
+int userNumberA = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите квадрат числа \"а\": ");
+int userNumberB = int.Parse(Console.ReadLine() ?? "");
 
-// if ((int)Math.Pow(userNumberA, 2) == userNumberB)
-// {
-//     Console.WriteLine($"Да {userNumberA} в квадрате будет {userNumberB} ");
-// }
-// else
-// {
-//     Console.WriteLine($"Нет {userNumberA} в квдрате не будет равняться {userNumberB} ");
-// }
+if ((int)Math.Pow(userNumberA, 2) == userNumberB)
+{
+    Console.WriteLine($"Да {userNumberA} в квадрате будет {userNumberB} ");
+}
+else
+{
+    Console.WriteLine($"Нет {userNumberA} в квдрате не будет равняться {userNumberB} ");
+}
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Проверка дней недели по числу
