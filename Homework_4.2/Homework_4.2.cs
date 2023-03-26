d24117c6422140ac46b64c73287d2ae7bf873d23
Dmitriy Gusev <gusdima228@gmail.com>
2023-03-27 01:12:12 +0300

    added homework_4.2 and Homework_4.3

---
+++ Homework_4.2/Homework_4.2.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+
+int numberN = GetNumberFromUser("Введите целое число N: ", "Ошибка ввода!");
+
+int sumNumber = SumNumber(numberN);
+
+Console.WriteLine("Сумма цифр в числе: " + sumNumber);
+
+
+int SumNumber(int numberN)
+{
+    int counter = Convert.ToString(numberN).Length;
+    int advance = 0;
+    int result = 0;
+
+    for (int i = 0; i < counter; i++)
+    {
+        advance = numberN - numberN % 10;
+        result = result + (numberN - advance);
+        numberN = numberN / 10;
+    }
+    return result;
+}
+
+
+int GetNumberFromUser(string massage, string errorMassage)
+{
+    while(true)
+    {
+        Console.Write(massage);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int UserNumber);
+        if(isCorrect && UserNumber >= 0 )
+            return UserNumber;
+        Console.Write(errorMassage);
+    }
+}
