b68c1ab7ded8b452a3bc9bee9132fbcbef944a7c
Dmitriy Gusev <gusdima228@gmail.com>
2023-03-27 00:25:24 +0300

    added homewor4.1 and Seminar_4

---
+++ Homework_4.1/Homework_4.1.cs
@@ -0,0 +1,38 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+int a = 0, b = 0, c = 1;
+InitCoordinatesByUser(ref a, ref b);
+
+
+if (b < 1)
+    Console.Write("Число B должно быть натуральным\n");
+else
+{
+    while (b != 0)
+    {
+        c = c * a;
+        b = b - 1;
+    }
+    Console.Write($"Число A в натуральной степени B равно {c}");
+}
+
+
+static void InitCoordinatesByUser(ref int a, ref int b)
+{   
+    while(true)
+    {
+        try
+        {
+            Console.Write("Введите число a: ");
+            a = int.Parse(Console.ReadLine() ?? "");
+            Console.Write("Введите число b: ");
+            b = int.Parse(Console.ReadLine() ?? "");
+            break;
+        }
+        catch(Exception)
+        {
+            Console.WriteLine($"Ошибка ввода данных!");
+        }
+    }
+}
---
+++ Seminar_4/Seminar_4.cs
@@ -0,0 +1,75 @@
+﻿// // Input
+
+// int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+
+// // Logic
+
+// int SumNumber = GetSumNumber(num);
+
+// // Output
+
+// Console.Write($"{num} -> {SumNumber}");
+
+// //////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// int GetSumNumber(int numbers)
+// {
+//     int sum = 0;
+
+//     while(numbers > 0)
+//     {
+//         sum += numbers;
+//         numbers--;
+//     }
+//     return sum;
+// }
+
+
+// int GetNumberFromUser(string massage, string errorMassage)
+// {
+//     while(true)
+//     {
+//         Console.Write(massage);
+//         bool isCorrect = int.TryParse(Console.ReadLine(), out int UserNumber);
+//         if(isCorrect && UserNumber >= 0 )
+//             return UserNumber;
+//         Console.Write(errorMassage);
+//     }
+// }
+
+// Программа которая выдает колличество цифр в введенном числе
+
+Console.Clear();
+
+int num = GetNumberFromUser("Введите целое число А: ", "Ошибка ввода!");
+
+int numDigit = CountDigit(num);
+
+Console.Write($"{num} -> {numDigit}");
+
+
+int CountDigit(int num)
+{
+    int count = 0;
+
+    while(num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+
+int GetNumberFromUser(string massage, string errorMassage)
+{
+    while(true)
+    {
+        Console.Write(massage);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int UserNumber);
+        if(isCorrect && UserNumber >= 0 )
+            return UserNumber;
+        Console.Write(errorMassage);
+    }
+}
+
